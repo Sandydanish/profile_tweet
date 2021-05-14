d056e17e94681d4dec60af019b2faab457178c8f
abc <abc@example.com>
2021-05-14 15:18:25 +0700

    Added new file

---
+++ lib/main.dart
@@ -0,0 +1,257 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MaterialApp(home: ProfilePage()));
+}
+
+class ProfilePage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+        title: Text(
+          "Profile Page",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: false,
+        children: <Widget>[
+          Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.bottomLeft,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 3.7,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage(
+                    "images/1.jpg",
+                  ),
+                  fit: BoxFit.cover,
+                )),
+              ),
+              Positioned(
+                top: 160,
+                left: 5,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                      image: DecorationImage(image: AssetImage("images/1.jpg")),
+                      border: Border.all(color: Colors.black, width: 5)),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "idName",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 22),
+                ),
+                Text(
+                  "idEmail",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Machine Learning an Data Science enthusiast Android Developer and always willing to learn and work with new technologies.",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.link,
+                      color: Colors.grey,
+                      size: 25,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "idName.herokuapp.com",
+                      style: TextStyle(color: Colors.green, fontSize: 19),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.panorama_fish_eye,
+                      color: Colors.grey,
+                      size: 25,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "1 January, 2001",
+                      style: TextStyle(color: Colors.grey, fontSize: 19),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.calendar_today,
+                      color: Colors.grey,
+                      size: 25,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Joined April 2016",
+                      style: TextStyle(color: Colors.grey, fontSize: 19),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "Tweets",
+                      style: TextStyle(color: Colors.green, fontSize: 18),
+                    ),
+                    SizedBox(
+                      width: 18,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: Container(
+                            height: 50,
+                            width: 50,
+                            child: CircleAvatar(
+                              backgroundImage: AssetImage("images/1.jpg"),
+                            )),
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Text(
+                                  "idName",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 22),
+                                )
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "I create UI in flutter. let me know what you think and give your feebbacks",
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 17,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.mode_comment,
+                                  color: Colors.grey,
+                                ),
+                                SizedBox(
+                                  width: 50,
+                                ),
+                                Icon(
+                                  Icons.repeat,
+                                  color: Colors.grey,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "2k",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                SizedBox(
+                                  width: 50,
+                                ),
+                                Icon(
+                                  Icons.favorite,
+                                  color: Colors.red,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "1.6k",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
